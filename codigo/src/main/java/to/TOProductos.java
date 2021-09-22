5ddfca47ad1b70511360c2ce03b8248d854e138e
Maria-Prada <mariaprada06@hotmail.com>
2021-09-22 18:02:43 -0500

    Update TOProductos.java

--- codigo/src/main/java/to/TOProductos.java
+++ codigo/src/main/java/to/TOProductos.java
@@ -6,14 +6,16 @@ public class TOProductos {
   private  int idProducto;
   private String nombreProducto;
   private String categoria;
+  private String unidadMedida;
   private int cantidad;
   private Double valorUnitarioCompra;
   private Double valorUnitarioVenta;
 
-    public TOProductos(int idProducto, String nombreProducto, String categoria, int cantidad, Double valorUnitarioCompra, Double valorUnitarioVenta) {
+    public TOProductos(int idProducto, String nombreProducto, String categoria,String unidadMedida, int cantidad, Double valorUnitarioCompra, Double valorUnitarioVenta) {
         this.idProducto = idProducto;
         this.nombreProducto = nombreProducto;
         this.categoria = categoria;
+        this.unidadMedida = unidadMedida;
         this.cantidad = cantidad;
         this.valorUnitarioCompra = valorUnitarioCompra;
         this.valorUnitarioVenta = valorUnitarioVenta;
@@ -32,6 +34,9 @@ public class TOProductos {
 
     public String getCategoria() {
         return categoria;
+    }
+    public String getUnidadMedida() {
+        return unidadMedida;
     }
 
     public int getCantidad() {
@@ -56,6 +61,9 @@ public class TOProductos {
 
     public void setCategoria(String categoria) {
         this.categoria = categoria;
+    }
+    public void setUnidadMedida(String unidadMedida) {
+        this.unidadMedida= unidadMedida;
     }
 
     public void setCantidad(int cantidad) {
