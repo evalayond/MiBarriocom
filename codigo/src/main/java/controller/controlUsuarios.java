1690ecdc5a3356bd790a31dea1d3d1ce7d03b658
Maria-Prada <mariaprada06@hotmail.com>
2021-09-22 20:59:05 -0500

    Update controlUsuarios.java

--- codigo/src/main/java/controller/controlUsuarios.java
+++ codigo/src/main/java/controller/controlUsuarios.java
@@ -1,23 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controller;
 
 import dao.DAOUsuarios;
 import to.TOUsuarios;
 import java.util.ArrayList;
 
-/**
- *
- * @author María
- */
 public class controlUsuarios {
     DAOUsuarios UsuariosDAO = new DAOUsuarios();
     
     public boolean insertUsuario(TOUsuarios TOUsuario){
-        return UsuariosDAO.insentarUsuario(TOUsuario);
+        return UsuariosDAO.insertarUsuario(TOUsuario);
     }
     
     public boolean actualizarUsuario(TOUsuarios TOUsuario){
@@ -29,6 +21,6 @@ public class controlUsuarios {
     }
     
     public ArrayList<TOUsuarios> listaUsuarios(){
-        return UsuariosDAO.listaUsuarios(0);
+        return UsuariosDAO.listaUsuarios();
     }
 }
