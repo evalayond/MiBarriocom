76c0558070d1f6c54cb889fcbc851d39aea71550
GaboCG <77843719+GaboCG@users.noreply.github.com>
2021-09-30 07:05:15 -0500

    Update controlLogin.java

--- codigo/src/main/java/controller/controlLogin.java
+++ codigo/src/main/java/controller/controlLogin.java
@@ -1,28 +1,17 @@
-
 package controller;
 
 import dao.DAOLogin;
+import dao.DAOVendedores;
 import java.util.ArrayList;
 import to.TOLogin;
 import to.TOVendedores;
 
 public class controlLogin {
-    DAOLogin loginDAO = new DAOLogin();
-    
-    public boolean insertLogin(TOLogin TOLog){
-        return loginDAO.insertarLogin(TOLog);
-    }
-    
-    public boolean actualizarLogin(TOLogin TOLog){
-        return loginDAO.actualizarLogin(TOLog);
-    }
-    
-    public boolean eliminarLogin(int idLogin){
-        return loginDAO.eliminarLogin(idLogin);
-    }
-    
-    public ArrayList<TOLogin> listaLogin(){
-        return loginDAO.listaLogin();
-    }
-}
 
+    DAOVendedores vendedoresDAO = new DAOVendedores();
+
+    public TOVendedores login(String usuario, String clave) {
+        return vendedoresDAO.login(usuario, clave);
+    }
+
+}
